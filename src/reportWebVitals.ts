648ef9bc841715fe4d9981e86bfea49e458691b5
DelfinaFenocchio <delfifenocchio@gmail.com>
2023-03-13 13:12:47 -0300

    disable jsdoc in reportwebvitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,3 +1,8 @@
+/* eslint-disable require-jsdoc */
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
 import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
